35aec5f6ab7929e5a80ac3913d7ea1ffa46c2eac
guilhermesallesf <guilhermesallesfonseca@gmail.com>
2025-05-28 11:44:18 -0300

    first commit

---
+++ doc/2.py
@@ -0,0 +1,2 @@
+n1=int(input("escreve um número:"))
+print("seu número é:",n1)
---
+++ doc/3.py
@@ -0,0 +1,6 @@
+n1=int(input("ponha um número aí carai:"))
+n2=int(input("põe mai um porra:"))
+
+nm=n1+n2
+
+print("se tu amuntua esses número aí da esse trem aqui ó:", nm)
